83ce7f39443775100365a30e99cf64e53df89644
aryanjoshi <127735906+nuerocist@users.noreply.github.com>
2026-08-12 02:30:00 +0000

    Make medication class and variables

--- Version 2.py
+++ Version 2.py
@@ -2,3 +2,11 @@ import json
 import os
 
 DATA_FILE = "medications.json"
+
+class Medication:
+    def __init__(self, name, time, dosage, taken=False):
+        self.name = name
+        self.time = time
+        self.dosage = dosage
+        self.taken = taken
+
